7d1ee8575627b845fef8f500342d33c746d09549
teppeis <teppeis@gmail.com>
2018-10-14 18:44:05 +0900

    test: use nock

--- test/index.js
+++ test/index.js
@@ -9,9 +9,6 @@ require('axios-debug-log');
 
 /** @test {RedashClient} */
 describe('RedashClient', () => {
-  beforeEach(nockBackMocha.beforeEach);
-  afterEach(nockBackMocha.afterEach);
-
   it('should be a constructor', () => {
     const client = new Client();
     assert(client instanceof Client);
@@ -20,18 +17,22 @@ describe('RedashClient', () => {
   describe('api', () => {
     let client;
 
-    beforeEach(() => {
+    beforeEach(function() {
       client = new Client({
         endPoint: 'https://demo.redash.io/',
         apiToken: process.env.API_TOKEN || 'abc123',
       });
+      return nockBackMocha.beforeEach.call(this);
     });
+
+    afterEach(nockBackMocha.afterEach);
 
     /** @test {RedashClient#getQuery} */
     it('getQuery', async () => {
       const actual = await client.getQuery(1);
       const expectedBody = require(back.fixtureFile)[0].response;
       assert.deepEqual(actual, expectedBody);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#getQueries} */
@@ -39,6 +40,7 @@ describe('RedashClient', () => {
       const actual = await client.getQueries();
       const expectedBody = require(back.fixtureFile)[0].response;
       assert.deepEqual(actual, expectedBody);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#postQuery} */
@@ -50,6 +52,7 @@ describe('RedashClient', () => {
       });
       const expectedBody = require(back.fixtureFile)[0].response;
       assert.deepEqual(actual, expectedBody);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#getJob} */
@@ -58,50 +61,39 @@ describe('RedashClient', () => {
       const actual = await client.getJob(id);
       const expectedBody = require(back.fixtureFile)[0].response;
       assert.deepEqual(actual, expectedBody);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#getQueryResult} */
-    it.skip('getQueryResult', async () => {
-      const id = 4578008;
+    it('getQueryResult', async () => {
+      const id = 4766466;
       const actual = await client.getQueryResult(id);
       const expectedBody = require(back.fixtureFile)[0].response;
       assert.deepEqual(actual, expectedBody);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#queryAndWaitResult} */
-    it.skip('queryAndWaitResult', async function() {
-      this.timeout(10000);
-      // const job1 = require('./fixtures/post-query_results-max_age_0');
-      // const job2 = require('./fixtures/get-jobs-2');
-      // const job3 = require('./fixtures/get-jobs-3');
-      // const queryResult = require('./fixtures/get-query_results');
-
-      // const jobId = job1.job.id;
-      // const queryResultId = job3.job.query_result_id;
-      // mock.onPost('/api/query_results').reply(200, job1);
-      // mock.onGet(`/api/jobs/${jobId}`).replyOnce(200, job2);
-      // mock.onGet(`/api/jobs/${jobId}`).replyOnce(200, job3);
-      // mock.onGet(`/api/query_results/${queryResultId}`).reply(200, queryResult);
-
+    it('queryAndWaitResult', async () => {
+      client = new Client({
+        endPoint: 'http://localhost:5000/',
+        apiToken: process.env.API_TOKEN || 'abc123',
+      });
       const actual = await client.queryAndWaitResult({
-        query: 'select * from cohort2',
-        data_source_id: 2,
+        query: '1E7lGYcgerhmYmKsfN7ReRulItby_tm8ivfTvuQD-TBM',
+        data_source_id: 1,
         max_age: 0,
       });
-      assert.deepEqual(actual, {});
+      const requests = require(back.fixtureFile);
+      const lastRequest = requests[requests.length - 1];
+      assert(/^\/api\/query_results\/\d+/.test(lastRequest.path));
+      assert(lastRequest.status === 200);
+      assert.deepEqual(actual, lastRequest.response);
+      back.assertScopesFinished();
     });
 
     /** @test {RedashClient#queryAndWaitResult} */
-    it.skip('queryAndWaitResult: timeout', function(done) {
-      this.timeout(3000);
-
-      // const job1 = require('./fixtures/post-query_results-max_age_0');
-      // const job2 = require('./fixtures/get-jobs-2');
-      //
-      // const jobId = job1.job.id;
-      // mock.onPost('/api/query_results').reply(200, job1);
-      // mock.onGet(`/api/jobs/${jobId}`).reply(200, job2);
-
+    it('queryAndWaitResult: timeout', () =>
       client
         .queryAndWaitResult(
           {
@@ -111,10 +103,12 @@ describe('RedashClient', () => {
           },
           1000
         )
-        .catch(e => {
-          assert(/polling timeout/.test(e.message));
-          done();
-        });
-    });
+        .then(
+          () => assert.fail('should be rejected'),
+          e => {
+            assert(/polling timeout/.test(e.message));
+            back.assertScopesFinished();
+          }
+        ));
   });
 });
